5feb753df6f98248c229aafabb267b52dc438e7e
Allan Agüero <aguero0810@hotmail.com>
2025-06-08 12:58:48 -0600

    Se incluye SP_CreateMovie y se realizan pruebas de funcionabilidad.

---
+++ DataAccess/Database Scripts/SP_CreateMovie.sql
@@ -0,0 +1,15 @@
+﻿--SP para crear una película.
+CREATE PROCEDURE CRE_MOVIE_PR
+(
+	@P_Title nvarchar(75),
+	@P_Description nvarchar(50),
+	@P_ReleaseDate DateTime,
+	@P_Genre nvarchar(20),
+	@P_Director nvarchar(30)
+)
+AS
+BEGIN
+	INSERT INTO TBL_Movie(Created, Title, Description, ReleaseDate, Genre, Director)
+	VALUES (GETDATE(), @P_Title, @P_Description, @P_ReleaseDate, @P_Genre, @P_Director);
+END
+GO
--- DevTester/Program.cs
+++ DevTester/Program.cs
@@ -9,17 +9,26 @@ public class Program
     {
         var sqlOperation = new SqlOperation();
 
-        sqlOperation.ProcedureName = "CRE_USER_PR";
+        /*sqlOperation.ProcedureName = "CRE_USER_PR";
 
         sqlOperation.AddStringParameter("P_UserCode", "aaguero");
         sqlOperation.AddStringParameter("P_Name", "Allan");
         sqlOperation.AddStringParameter("P_Email", "aaguero@ucenfotec.ac.cr");
         sqlOperation.AddStringParameter("P_Password", "Cenfotec123!");
         sqlOperation.AddStringParameter("P_Status", "AC");
-        sqlOperation.AddDateTimeParam("P_BirthDate", DateTime.Now);
+        sqlOperation.AddDateTimeParam("P_BirthDate", DateTime.Now);*/
+
+        sqlOperation.ProcedureName = "CRE_MOVIE_PR";
+
+        sqlOperation.AddStringParameter("P_Title", "The Matrix");
+        sqlOperation.AddStringParameter("P_Description", "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.");
+        sqlOperation.AddDateTimeParam("P_ReleaseDate", new DateTime(1999, 3, 31));
+        sqlOperation.AddStringParameter("P_Genre", "Sci-Fi");
+        sqlOperation.AddStringParameter("P_Director", "Lana Wachowski, Lilly Wachowski");
 
         var sqlDao = SqlDao.GetInstance();
 
         sqlDao.ExecuteProcedure(sqlOperation);
+
     }
 }

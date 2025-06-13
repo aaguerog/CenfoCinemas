a4c46967a39de05bd8cf9275dc8d7005f4c7a496
Allan Agüero <aguero0810@hotmail.com>
2025-06-12 21:42:41 -0600

    Creacion de Menu en Program, inclusion de clases CrudFactory, Movie y User en carpeta CRUD

---
+++ DataAccess/CRUD/CrudFactory.cs
@@ -0,0 +1,34 @@
+﻿using DataAccess.DAOs;
+using DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.CRUD
+{
+    public abstract class CrudFactory
+
+    //Clase padre/madre de los cruds
+    //Define como se hacen cruds en la arquitectura
+
+    {
+        protected SqlDao _sqlDao;
+
+        //Definir los metodos que forman parte del contrato
+        // C = Create
+        // R = Retrieve
+        // U = Update
+        // D = Delete
+
+        public abstract void Create(BaseDTO baseDTO);
+        public abstract void Update(BaseDTO baseDTO);
+        public abstract void Delete(BaseDTO baseDTO);
+
+        public abstract T Retrieve<T>();
+        public abstract T RetrieveById<T>();
+        public abstract List<T> RetrieveAll<T>();
+
+    }
+}
--- DevTester/Program.cs
+++ DevTester/Program.cs
@@ -1,34 +1,131 @@
 ﻿
 
+using DataAccess.CRUD;
 using DataAccess.DAO;
 using DataAccess.DAOs;
+using DTOs;
+using Newtonsoft.Json;
 
 public class Program
 {
     public static void Main(string[] args)
     {
+
+        //Crear un menu y capturar la data de consola
+
         var sqlOperation = new SqlOperation();
 
-        /*sqlOperation.ProcedureName = "CRE_USER_PR";
+        Console.WriteLine("Bienvenido a CenfoCinemas");
+        Console.WriteLine("Seleccione una opción:");
+        Console.WriteLine("1. Crear usuario");
+        Console.WriteLine("2. Consultar usuarios");
+        Console.WriteLine("3. Actualizar usuario");
+        Console.WriteLine("4. Eliminar usuario");
+        Console.WriteLine("5. Agregar película");
+        Console.WriteLine("6. Consultar películas");
+        Console.WriteLine("7. Actualizar película");
+        Console.WriteLine("8. Eliminar película");
+        Console.WriteLine("9. Salir");
 
-        sqlOperation.AddStringParameter("P_UserCode", "aaguero");
-        sqlOperation.AddStringParameter("P_Name", "Allan");
-        sqlOperation.AddStringParameter("P_Email", "aaguero@ucenfotec.ac.cr");
-        sqlOperation.AddStringParameter("P_Password", "Cenfotec123!");
-        sqlOperation.AddStringParameter("P_Status", "AC");
-        sqlOperation.AddDateTimeParam("P_BirthDate", DateTime.Now);*/
+        var option = Int32.Parse(Console.ReadLine());
 
-        sqlOperation.ProcedureName = "CRE_MOVIE_PR";
+        switch (option)
+        {
+            case 1:
+                Console.WriteLine("-- Crear Usuario --");
 
-        sqlOperation.AddStringParameter("P_Title", "The Matrix");
-        sqlOperation.AddStringParameter("P_Description", "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.");
-        sqlOperation.AddDateTimeParam("P_ReleaseDate", new DateTime(1999, 3, 31));
-        sqlOperation.AddStringParameter("P_Genre", "Sci-Fi");
-        sqlOperation.AddStringParameter("P_Director", "Lana Wachowski, Lilly Wachowski");
+                Console.Write("Código de usuario: ");
+                var userCode = Console.ReadLine();
 
-        var sqlDao = SqlDao.GetInstance();
+                Console.Write("Nombre: ");
+                var name = Console.ReadLine();
 
-        sqlDao.ExecuteProcedure(sqlOperation);
+                Console.Write("Correo electrónico: ");
+                var email = Console.ReadLine();
+
+                Console.Write("Contraseña: ");
+                var password = Console.ReadLine();
+
+                var status = "AC";
+
+                Console.Write("Fecha de nacimiento (yyyy-MM-dd): ");
+                DateTime birthDate;
+                while (!DateTime.TryParse(Console.ReadLine(), out birthDate))
+                {
+                    Console.Write("Formato incorrecto. Intente de nuevo: ");
+                }
+
+                var user = new User()
+                {
+                    UserCode = userCode,
+                    Name = name,
+                    Email = email,
+                    Password = password,
+                    Status = status,
+                    BirthDate = birthDate
+                };
+
+                var uCrud = new UserCrudFactory();
+                uCrud.Create(user);
+
+                break;
+
+            case 2:
+
+                Console.WriteLine("-- Consultar Usuarios --");
+
+                uCrud = new UserCrudFactory();
+                var listUsers = uCrud.RetrieveAll<User>();
+
+                foreach(var u in listUsers)
+                { 
+                    Console.WriteLine(JsonConvert.SerializeObject(u));
+                }
+
+                break;
+
+            case 5:
+                Console.WriteLine("-- Agregar Película --");
+
+                Console.Write("Título: ");
+                var title = Console.ReadLine();
+
+                Console.Write("Descripción: ");
+                var description = Console.ReadLine();
+
+                Console.Write("Fecha de estreno (yyyy-MM-dd): ");
+                DateTime releaseDate;
+
+                while (!DateTime.TryParse(Console.ReadLine(), out releaseDate))
+                {
+                    Console.Write("Formato incorrecto. Intente de nuevo: ");
+                }
+
+                Console.Write("Género: ");
+                var genre = Console.ReadLine();
+
+                Console.Write("Director: ");
+                var director = Console.ReadLine();
+
+                sqlOperation.ProcedureName = "CRE_MOVIE_PR";
+                sqlOperation.AddStringParameter("P_Title", title);
+                sqlOperation.AddStringParameter("P_Description", description);
+                sqlOperation.AddDateTimeParam("P_ReleaseDate", releaseDate);
+                sqlOperation.AddStringParameter("P_Genre", genre);
+                sqlOperation.AddStringParameter("P_Director", director);
+
+                break;
+
+            case 9:
+                Console.WriteLine("Saliendo del sistema.");
+                break;
+
+            default:
+                Console.WriteLine("Opción no válida.");
+                break;
+        }
+
+
 
     }
 }
